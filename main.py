02bade7cb0a4ba7f7bed381e5ea12751a09332a6
TheaLeNeveu <Thea.LeNeveu@laetottenham.ac.uk>
2024-11-11 12:28:02 +0000

    hello

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ class cat:
         self.energy = 50
         self.intelligence = 5
         self.weight = 5
+        self.friendship = 0
     
     def train(self):
-        print(f"{self.name} is training")
-        self.energy -= 5
-        self.intelligence += 1
-        self.age += 0.1
+        if cat_attributes["energy"] < 7:
+            print(name, "is too tired to train right now.")
+        else:
+            print(f"{self.name} is training")
+            self.friendship += 10
+            self.intelligence += 20
+            self.energy"] -= 20
+            cat_attributes["weight"] -= 5
+            print(name, "gained 10 friendship points")
+            print(name, "gained 20 intelligence points")
+            print(name, "lost 20 energy points")
+            print(name, "lost 5 weight points")
 
     def feed(self):
         print(f"{self.name} is eating")
-        sel.energy += 10
-        self.weight += 1
-        self.age += 0.1
+        cat_attributes["friendship"] += 20
+        cat_attributes["energy"] += 10
+        cat_attributes["weight"] += 5
+        print(name, "gained 20 friendship points")
+        print(name, "gained 10 energy points")
+        print(name, "gained 5 weight points")
 
 #We will now make 2 cats.
 sid = cat("Sid", "Black")
@@ -40,3 +52,68 @@ while True:
         cat.train()
     elif action == "2":
         cat.feed()
+
+options = [
+    {
+        "type": "list",
+        "message": "What would you like to do?",
+        "choices": ["Play with your cat", "Train your cat", "Feed your cat", "Let your cat sleep", "Show stats"]
+    }
+]
+
+cat_attributes = {
+    "friendship": 0,
+    "intelligence": 10,
+    "energy": 50,
+    "weight": 10,
+    # change the inital values above
+}
+
+print("Welcome to my cat game!")
+
+# Take the user inputs for name and colour:
+ans = prompt(questions)
+name = ans["name"]
+colour = ans["colour"]
+
+# start the while loop
+while True:
+    # Finish the string below
+    option = prompt(options)[0]
+    if option == "Play with your cat":
+        if cat_attributes["energy"] < 5:
+            print(name, "is too tired to play right now.")
+        # change the cat's attributes here
+        else:
+            cat_attributes["friendship"] += 30
+            cat_attributes["intelligence"] += 5
+            cat_attributes["energy"] -= 10
+            cat_attributes["weight"] -= 5
+            print(name, "gained 10 friendship points")
+            print(name, "gained 5 intelligence points")
+            print(name, "lost 10 energy points")
+            print(name, "lost 5 weight points")
+
+    elif option == "Train your cat":
+        
+
+    elif option == "Feed your cat":
+        
+
+    elif option == "Let your cat sleep":
+        cat_attributes["energy"] += 50
+        cat_attributes["weight"] += 5
+        print(name, "gained 50 energy points")
+        print(name, "gained 5 weight points")
+
+    elif option == "Show stats":
+        print(cat_attributes)
+    # elif ...
+    else:
+        pass
+
+    # finish off the if statements below
+    if cat_attributes['energy'] < 0:
+        print(name, "died from exhaustion.")
+    # elif ...
+    
